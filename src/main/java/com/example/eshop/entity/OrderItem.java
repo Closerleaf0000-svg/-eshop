51019ac6e921ff181fc6054b846b0921f9c84f07
Closerleaf0000-svg <soisgreat0000@gmail.com>
2026-08-17 14:54:07 +0800

    Create OrderItem.java

---
+++ src/main/java/com/example/eshop/entity/OrderItem.java
@@ -0,0 +1,75 @@
+package com.example.eshop.entity;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private Long orderItemId;
+    private Long orderId;
+    private Long productId;
+    private String productName;
+    private String productImage;
+    private Integer quantity;
+    private BigDecimal productPrice;
+
+    public OrderItem() {
+
+    }
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(Long orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+
+}
